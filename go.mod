3d82a0875139c3c967af5942a976312cc1fc19cb
Luke Johnston <lukejohnston@outlook.com>
2020-01-08 15:06:38 -0800

    Test selection

--- go.mod
+++ go.mod
@@ -1,12 +1,11 @@
 module github.com/lukejohnston/egotest
 
 require (
-	github.com/gdamore/encoding v0.0.0-20151215212835-b23993cbb635 // indirect
-	github.com/gdamore/tcell v1.1.0
-	github.com/lucasb-eyer/go-colorful v0.0.0-20181028223441-12d3b2882a08 // indirect
-	github.com/mattn/go-runewidth v0.0.4-0.20181117163326-c88d7e5f2e24 // indirect
-	github.com/rivo/tview v0.0.0-20181126113627-0a18dea45814
-	golang.org/x/text v0.3.0 // indirect
+	github.com/gdamore/tcell v1.3.0
+	github.com/lucasb-eyer/go-colorful v1.0.3 // indirect
+	github.com/mattn/go-runewidth v0.0.7 // indirect
+	github.com/rivo/tview v0.0.0-20191231100700-c6236f442139
+	golang.org/x/sys v0.0.0-20200106162015-b016eb3dc98e // indirect
 )
 
 go 1.13
--- main.go
+++ main.go
@@ -21,19 +21,34 @@ type TestLine struct {
 	Output  string
 }
 
+var dir string
+var app *tview.Application
+
 func main() {
-	dir := ""
+	f, err := os.OpenFile("access.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.SetOutput(f)
+
+	dir = ""
 	if len(os.Args) > 1 {
 		dir = os.Args[1]
 	}
 
+	selectedTests := make(map[*tview.TreeNode]bool)
 	root := tview.NewTreeNode("")
 	tree := tview.NewTreeView().SetRoot(root).SetCurrentNode(root)
-	app := tview.NewApplication().SetRoot(tree, true)
+	app = tview.NewApplication().SetRoot(tree, true)
 
 	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		if event.Key() == tcell.KeyRune && event.Rune() == 'q' {
-			app.Stop()
+		if event.Key() == tcell.KeyRune {
+			if event.Rune() == 'q' {
+				app.Stop()
+			} else if event.Rune() == 'r' {
+				go runSelectedTests(selectedTests)
+			}
 		}
 
 		return event
@@ -61,7 +76,7 @@ func main() {
 				root.AddChild(packageNode)
 			}
 
-			node := tview.NewTreeNode(testLine.Output).SetReference(testLine.Output).SetSelectable(true)
+			node := tview.NewTreeNode(fmt.Sprintf("  ( ) %s", testLine.Output)).SetReference(testLine.Output).SetSelectable(true)
 			packageNode.AddChild(node)
 		}
 	}
@@ -74,40 +89,62 @@ func main() {
 			return
 		}
 
-		reference := node.GetReference()
-		if reference == nil {
-			return
+		_, ok := selectedTests[node]
+		if ok {
+			delete(selectedTests, node)
+			app.QueueUpdateDraw(func() { node.SetText(replaceAtIndex(node.GetText(), ' ', 3)) })
+		} else {
+			selectedTests[node] = true
+			app.QueueUpdateDraw(func() { node.SetText(replaceAtIndex(node.GetText(), 'x', 3)) })
 		}
-
-		testName := reference.(string)
-
-		node.SetText(fmt.Sprintf("R %s", testName))
-
-		go func() {
-			raw, err := exec.Command("go", "test", "-run", testName, "-json", dir).Output()
-			if err != nil {
-				panic(err)
-			}
-
-			scanner := bufio.NewScanner(bytes.NewReader(raw))
-			for scanner.Scan() {
-				testLine := TestLine{}
-				err := json.Unmarshal(scanner.Bytes(), &testLine)
-				if err != nil {
-					panic(err)
-				}
-
-				if testLine.Action == "pass" {
-					app.QueueUpdateDraw(func() { node.SetText(fmt.Sprintf("P %s", testName)) })
-				} else if testLine.Action == "fail" {
-					app.QueueUpdateDraw(func() { node.SetText(fmt.Sprintf("F %s", testName)) })
-				}
-			}
-		}()
 	})
 
 	if err := app.Run(); err != nil {
 		panic(err)
 	}
+}
 
+func replaceAtIndex(in string, r rune, i int) string {
+	out := []rune(in)
+	out[i] = r
+	return string(out)
+}
+
+func runSelectedTests(selectedTests map[*tview.TreeNode]bool) {
+	for testNode, _ := range selectedTests {
+		thisTestNode := testNode
+		app.QueueUpdateDraw(func() { thisTestNode.SetText(replaceAtIndex(thisTestNode.GetText(), 'R', 0)) })
+	}
+
+	for testNode, _ := range selectedTests {
+		runTest(testNode)
+	}
+}
+func runTest(node *tview.TreeNode) {
+	reference := node.GetReference()
+	if reference == nil {
+		return
+	}
+
+	testName := reference.(string)
+
+	raw, err := exec.Command("go", "test", "-run", testName, "-json", dir).Output()
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(bytes.NewReader(raw))
+	for scanner.Scan() {
+		testLine := TestLine{}
+		err := json.Unmarshal(scanner.Bytes(), &testLine)
+		if err != nil {
+			panic(err)
+		}
+
+		if testLine.Action == "pass" {
+			app.QueueUpdateDraw(func() { node.SetText(replaceAtIndex(node.GetText(), 'P', 0)) })
+		} else if testLine.Action == "fail" {
+			app.QueueUpdateDraw(func() { node.SetText(replaceAtIndex(node.GetText(), 'F', 0)) })
+		}
+	}
 }
